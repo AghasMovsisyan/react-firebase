5a291e74410a482d0e4d8ef3709d8cd0f542ba3d
Aghas <aghas.movsisyan3@gmail.com>
2023-11-07 14:12:54 +0400

    update firebase.js(new database)

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "@firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD1yh8iRu-r7GnRxdr3xXzxNb8dkFyRLBc",
-  authDomain: "react1-322d9.firebaseapp.com",
-  projectId: "react1-322d9",
-  storageBucket: "react1-322d9.appspot.com",
-  messagingSenderId: "377925761959",
-  appId: "1:377925761959:web:65e7d4b01eafbeb5d95482",
-  measurementId: "G-LVY3N53KK1"
+  apiKey: "AIzaSyA2AWi8bFvILrUaFrbAb3j2BFsTv_Ol1io",
+  authDomain: "react2-fb5e9.firebaseapp.com",
+  projectId: "react2-fb5e9",
+  storageBucket: "react2-fb5e9.appspot.com",
+  messagingSenderId: "829834731327",
+  appId: "1:829834731327:web:e12f13a77c13284bc7bd8a",
+  measurementId: "G-JEJVBM5PKL"
 };
 
 const app = initializeApp(firebaseConfig);
